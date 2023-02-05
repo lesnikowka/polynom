95421ea73f73f810e82e066d6abdb185c81a16cc
Лесников Никита <lesnikowka@mail.ru>
2023-02-05 19:38:19 +0300

    add test for monom and fix state machine

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,23 +1,8 @@
 #include <gtest.h>
 
-#include "Polynom.h"
-
-void print_v(std::vector<size_t> v) {
-	for (auto i : v) {
-		std::cout << i << " ";
-	}
-	std::cout << std::endl;
-}
 
 int main(int argc, char **argv)
 {
-	std::string s = "x0^2";
-
-	Monom<3, 10> m(s);
-
-	std::cout << (m.get_coef()) << std::endl;
-	print_v(m.get_degree());
-
-  //::testing::InitGoogleTest(&argc, argv);
-  //return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
---
+++ test/test_monom.cpp
@@ -0,0 +1,95 @@
+#include <gtest.h>
+#include "Monom.h"
+
+class Fixture_Monom : public ::testing::Test {
+public:
+	Monom<3, 5> m, m1, m2, m3;
+	Fixture_Monom() = default;
+};
+
+TEST_F(Fixture_Monom, get_str_is_correct) {
+	m.parse("-1.23x0^2x2^1x1^0");
+
+	EXPECT_EQ(m.get_str(), "-1.23x0^2x2^1");
+}
+
+TEST_F(Fixture_Monom, get_deg_is_correct) {
+	m.parse("-1.23x0^2x2^1x1^0");
+
+	EXPECT_EQ(m.get_deg(), std::vector<size_t>({2,0,1}));
+}
+
+
+TEST_F(Fixture_Monom, get_coef_is_correct) {
+	m.parse("-1.23x0^2x2^1x1^0");
+
+	EXPECT_EQ(m.get_coef(), -1.23);
+}
+
+
+TEST_F(Fixture_Monom, can_calculate_monom) {
+	m.parse("-2x0^2x1^2x2^1");
+
+	EXPECT_EQ(m.calculate({ 2,3,3 }), -216);
+}
+
+
+TEST_F(Fixture_Monom, can_copy_monom) {
+	m.parse("-2x0^2x1^2x2^1");
+
+	Monom<3, 5> m4 = m;
+
+	EXPECT_TRUE(m == m4);
+}
+
+
+TEST_F(Fixture_Monom, can_assign_monom) {
+	m1.parse("-2x0^2x1^2x2^1");
+
+	Monom<3, 5> m2 = m1;
+
+	EXPECT_TRUE(m1 == m2);
+}
+
+
+TEST_F(Fixture_Monom, is_similar_is_correct) {
+	m1.parse("-1.23x0^0x1^1x2^2");
+	m2.parse("100x2^2x1^1");
+	m3.parse("-1.23x0^0x1^1x2^1");
+
+	EXPECT_TRUE(m1.is_similar(m2));
+	EXPECT_FALSE(m1.is_similar(m3));
+}
+
+
+TEST_F(Fixture_Monom, false_if_degree_is_too_big) {
+	EXPECT_FALSE(m1.parse("-1.23x0^0x1^10x2^2"));
+}
+
+TEST_F(Fixture_Monom, false_if_to_many_variables) {
+	EXPECT_FALSE(m1.parse("-1.23x0^0x1^10x3^2"));
+}
+
+//TEST_F(Fixture_Monom, ) {
+//
+//}
+
+
+//TEST_F(Fixture_Monom, ) {
+//
+//}
+
+
+//TEST_F(Fixture_Monom, ) {
+//
+//}
+
+
+//TEST_F(Fixture_Monom, ) {
+//
+//}
+
+
+//TEST_F(Fixture_Monom, ) {
+//
+//}
